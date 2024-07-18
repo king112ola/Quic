fc4632302b33d61d7ff143103a2ae75a71f17c8e
KIng <king94596511@gmail.com>
2024-07-18 20:55:12 +0800

    Moralis Force Rebuild
    Require the user to rebuild using npm run build to avoid OS compatibility issue.

--- Moralis-Server-Self-Host/build/cloud/upgradeRequest.d.ts
+++
@@ -1,3 +0,0 @@
-import { Operation } from '@moralisweb3/common-core';
-export type UnknownOperation = Operation<unknown, unknown, unknown, unknown>;
-export declare function upgradeRequest(params: any, operation: UnknownOperation): any;
